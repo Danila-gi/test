34eec0fa20350c06335878da09903f4b8945f78a
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2024-02-06 20:59:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,102 +1,39 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define TEXT_MAX_SIZE 10000
-#define NUMBER_OF_SENTENCES 1000
-#define END_OF_INPUT "Dragon flew away!"
 
-char* space(char* str){
-    char* res = calloc(strlen(str) + 1, sizeof(char));
-    int start = 0;
-    while (str[start] == '\t' || str[start] == ' '){
-        start++;
-    }
-    for (int i = 0; i < strlen(str) - start; i++){
-        res[i] = str[start + i];
-    }
-    res[strlen(str) - start] = '\0';
-    return res;
-}
-
-int hasNoSeven(char* str){
-    if (strstr(str, "7") == NULL){
-        return 1;
-    }
-    return 0;
-}
-
-char** allocate_memory_for_text(){
-    char **dyn_strs = malloc(NUMBER_OF_SENTENCES * sizeof(char*));
-    for(int i = 0; i < NUMBER_OF_SENTENCES; i++){
-        dyn_strs[i] = calloc(TEXT_MAX_SIZE, sizeof(char));
-    } 
-    return dyn_strs;
-}
-
-void reading_of_sentences(int* ptr_index, int* ptr_len, int* ptr_result, char** dyn_strs, char* str){
-    int overflow_str = 1;
-    int overflow_mas = 1;
-    while (1){
-        char symbol = getchar();
-        str[(*ptr_index)++] = symbol;
-        if (symbol == '.' || symbol == '?' || symbol == ';'){
-            str[*ptr_index] = '\0';
-            dyn_strs[(*ptr_len)++] = str;
-            *ptr_index = 0;
-            str = calloc(TEXT_MAX_SIZE, sizeof(char));
-        }
-        if (strstr(str, END_OF_INPUT) != NULL){
-            dyn_strs[(*ptr_len)++] = END_OF_INPUT;
-            break;
-        }
-        if (TEXT_MAX_SIZE * overflow_str <= (*ptr_index)){
-            str = realloc(str, TEXT_MAX_SIZE * (++overflow_str) * sizeof(char));
-            dyn_strs[(*ptr_len)] = realloc(dyn_strs[(*ptr_len)], TEXT_MAX_SIZE * overflow_str * sizeof(char));
-        }
-        if (NUMBER_OF_SENTENCES * overflow_mas <= (*ptr_len)){
-            dyn_strs = realloc(dyn_strs, NUMBER_OF_SENTENCES * (++overflow_mas) * sizeof(char*));
-        }
-    }
-}
-
-void output_of_sentences(int* ptr_len, int* ptr_result, char** dyn_strs){
-    for (int i = 0; i < (*ptr_len) - 1; i++){
-        char* sentence = space(dyn_strs[i]);
-        if (hasNoSeven(sentence)){
-            (*ptr_result)++;
-            printf("%s\n", sentence);
-        }
-        free(sentence);
-    }
-    printf("%s\n", dyn_strs[(*ptr_len) - 1]);
-}
-
-void free_memory(char** dyn_strs, int len){
-    for (int i = 0; i < len - 1; i++){
-        free(dyn_strs[i]);
-    }
-    free(dyn_strs);
-}
-
-int main()
-{
-    char **dyn_strs = allocate_memory_for_text();
-    char* str = calloc(TEXT_MAX_SIZE, sizeof(char));
+int main() {
+    char** text = malloc(sizeof(char*));
+    text[0] = calloc(1, sizeof(char));
+    int overflow_sent = 1;
+    int overflow_symb = 1;
+    char* sent = calloc(1, sizeof(char));
     int index = 0;
-    int* ptr_index = &index;
-
-    int len = 0;
-    int* ptr_len = &len;
-
-    int result = 0;
-    int* ptr_result = &result;
-
-    reading_of_sentences(ptr_index, ptr_len, ptr_result, dyn_strs, str);
-
-    output_of_sentences(ptr_len, ptr_result, dyn_strs);
-
-    free_memory(dyn_strs, len);
-
-    printf("Количество предложений до %d и количество предложений после %d", len - 1, result);
-    return 0;
+    char symbol;
+    while (1){
+        symbol = getchar();
+        if (symbol == '\n'){
+            overflow_sent++;
+            text[overflow_sent - 2] = sent;
+            sent = calloc(1, sizeof(char));
+            index = 0;
+            overflow_symb = 1;
+            if (strlen(text[overflow_sent - 2]) >= 4 && text[overflow_sent - 2][0] == 'F' && text[overflow_sent - 2][1] == 'i' && text[overflow_sent - 2][2] == 'n' && text[overflow_sent - 2][3] == '.'){
+                overflow_sent--;
+                break;
+            }
+            text = realloc(text, (overflow_sent) * sizeof(char*));
+            text[overflow_sent - 1] = calloc(1, sizeof(char));
+            continue;
+        }
+        sent[index++] = symbol;
+        overflow_symb++;
+        sent = realloc(sent, (overflow_symb) * sizeof(wchar_t));
+        text[overflow_sent - 1] = realloc(text[overflow_sent - 1], (overflow_symb) * sizeof(wchar_t));
+        text[overflow_sent - 1][overflow_symb - 1] = '\0';
+        sent[overflow_symb - 1] = '\0';
+    }
+    for (int i = 0; i < overflow_sent; i++){
+        printf("{%s}\n", text[i]);
+    }
 }
