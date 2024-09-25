1d306f2c95feba517034be32dcd07fb9912ba01d
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2024-09-25 16:09:32 +0300

    Update test_manager.cpp

--- oop/test_manager.cpp
+++ oop/test_manager.cpp
@@ -11,22 +11,25 @@ int main(){
     Length_of_the_ship l[] = {THREE, TWO, TWO, ONE};
 
     Manager_of_ships m = Manager_of_ships(4, l);
-    m.add_ship(&s1);
 
-    s1.shoot_to_segment(1);
-    m.get_arr_of_ships()[0]->shoot_to_segment(2);
-
-    m.add_ship(&s2);
+    m.add_ship(static_cast<Length_of_the_ship>(s1.get_length()), s1.get_location());
+    m.add_ship(static_cast<Length_of_the_ship>(s2.get_length()), s2.get_location());
     m.add_ship(ONE, Vertical);
-    m.add_ship(&s3);
+    m.add_ship(static_cast<Length_of_the_ship>(s3.get_length()), s3.get_location());
+
+    m.shoot_to_ship(0, 2);
+    m.shoot_to_ship(5, 0);
+
+    m.set_location_for_the_ship(0, Vertical);
+
     m.remove_ship(1);
     m.print_ships();
 
-    for (int i = 0; i < m.get_count(); i++)
-        std::cout<<m.get_arr_of_ships()[i]->get_length()<<":";
+    for (int i = 0; i < m.get_count_of_ships(); i++)
+        std::cout<<m.get_arr_of_ships()[i].get_length()<<":";
     std::cout<<std::endl;
 
-    std::cout<<m.get_count()<<std::endl;
+    std::cout<<m.get_count_of_ships()<<std::endl;
 
     return 0;
 }
