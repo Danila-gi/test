402c5e9715870edbd532d68bd628aa7f98738cef
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2024-10-20 21:50:05 +0300

    Update AtackException.h

--- oop2/headers/AtackException.h
+++ oop2/headers/AtackException.h
@@ -2,10 +2,14 @@
 #define ATACK_ERROR_H
 
 #include <iostream>
+#include <sstream>
 
 class AtackException : public std::runtime_error {
 public:
-    explicit AtackException(const std::string& message = "Incorrect coords for the atack!");
+    explicit AtackException(int x, int y);
+
+private:
+    std::string constructMessage(int x, int y) const;
 };
 
 #endif
