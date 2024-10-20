0d5acea92aa6d6f4c2b6c51f7fc2d57a9b7e8587
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2024-10-20 21:50:35 +0300

    Update ShipPlacmentException.h

--- oop2/headers/ShipPlacmentException.h
+++ oop2/headers/ShipPlacmentException.h
@@ -2,10 +2,14 @@
 #define INCERRECT_SHIP_COORDS_H
 
 #include <iostream>
+#include <sstream>
 
 class ShipPlacmentException : public std::runtime_error {
 public:
-    explicit ShipPlacmentException(const std::string& message = "Incorrect coords for the ship!");
+    explicit ShipPlacmentException(int x, int y);
+
+private:
+    std::string constructMessage(int x, int y) const;
 };
 
 #endif
