78173e56a14e4d3436308e1a342d853e01a513ff
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2024-10-20 21:49:16 +0300

    Update AtackException.cpp

--- oop2/includes/AtackException.cpp
+++ oop2/includes/AtackException.cpp
@@ -1,3 +1,10 @@
 #include "../headers/AtackException.h"
 
-AtackException::AtackException(const std::string& message): std::runtime_error(message) {}
+AtackException::AtackException(int x, int y)
+    : std::runtime_error(constructMessage(x, y)) {}
+
+std::string AtackException::constructMessage(int x, int y) const {
+    std::ostringstream oss;
+    oss << "Incorrect coords for the attack! Coordinates: (" << x << ", " << y << ")";
+    return oss.str();
+}
