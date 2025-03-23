ccb579c03bc475d0f53aed5262fafd00f8408c9b
3342.Ivanov.Danila <66697615+Danila-gi@users.noreply.github.com>
2025-03-23 20:23:54 +0300

    Update main.cpp

--- ПиАА/lab1/main.cpp
+++ ПиАА/lab1/main.cpp
@@ -1,153 +1,160 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <map>
 #include <algorithm>
-#include <tuple>
 
-/* Структура, описывающая элементы, которые будут
-храниться в стеке*/ 
-struct StackElement {
-    std::vector<std::vector<int>> grid; // матрица, описывающая состояние каждой клетки столешницы 
-    std::vector<std::tuple<int, int, int>> squaresUsed; // вектор использованных квадратов (координаты и размер)
-    std::pair<int, int> emptyCell; // пара чисел, хранящая следующие пустые клетки на столешнице
+struct State {
+    std::string name_of_block;
+    std::map<char, int> transitions;
+    int fail = -1;
+    std::vector<int> output;
 };
 
-void printMatrix(const std::vector<std::vector<int>>& grid) {
-    // Функция для вывода в консоль текущего состояния матрицы
-    for (const auto& row : grid) {
-        for (int val : row) {
-            std::cout << val << "\t";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
+class AhoCorasick {
+private:
+    std::vector<State> automaton;
+    int root = 0;
+    std::vector<std::string> patterns;
 
-bool isValid(int x, int y, int w, int n, const std::vector<std::vector<int>>& grid) {
-    // Функция, которая проверяет возможность поставить на координаты (x, y) квадрат размера w
-    if (x + w > n || y + w > n) {
-        return false;
+    void buildTrie() {
+        automaton.resize(1);
+        automaton[0].name_of_block = "";
+        for (int i = 0; i < patterns.size(); ++i) {
+            int current = root;
+            for (char ch : patterns[i]) {
+                if (automaton[current].transitions.find(ch) == automaton[current].transitions.end()) {
+                    automaton[current].transitions[ch] = automaton.size();
+                    automaton.emplace_back();
+                    automaton[automaton.size() - 1].name_of_block = automaton[current].name_of_block + ch;
+                }
+                current = automaton[current].transitions[ch];
+            }
+            automaton[current].output.push_back(i + 1);
+        }
+        /*int index = 0;
+        for (auto x: automaton){
+            std::cout << index++ << std::endl;
+            std::cout << "Block: " << x.name_of_block << std::endl;
+            for (auto y: x.transitions){
+                std::cout << y.first << " - " << y.second << std::endl;
+            }
+            for (auto y: x.output){
+                std::cout << y << " ";
+            }
+            std::cout << "\n=============\n";
+        }*/
     }
-    for (int i = x; i < x + w; i++) {
-        for (int j = y; j < y + w; j++) {
-            if (grid[i][j] != 0) {
-                return false;
+
+    void buildFailureLinks() {
+        std::queue<int> q;
+        for (auto& pair : automaton[root].transitions) {
+            q.push(pair.second);
+            automaton[pair.second].fail = root;
+        }
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+
+            for (auto& pair : automaton[current].transitions) {
+                //std::cout << "Current: " << current << " -- ";
+                //std::cout << pair.first << " | " << pair.second << std::endl;
+                char ch = pair.first;
+                int nextState = pair.second;
+                q.push(nextState);
+
+                int failState = automaton[current].fail;
+                //std::cout << failState << std::endl;
+                while (failState != -1 && automaton[failState].transitions.find(ch) == automaton[failState].transitions.end()) {
+                    failState = automaton[failState].fail;
+                    //std::cout << failState << std::endl;
+                }
+
+                if (failState == -1) {
+                    automaton[nextState].fail = root;
+                } else {
+                    automaton[nextState].fail = automaton[failState].transitions[ch];
+                }
+
+                automaton[nextState].output.insert(automaton[nextState].output.end(),
+                                                   automaton[automaton[nextState].fail].output.begin(),
+                                                   automaton[automaton[nextState].fail].output.end());
             }
         }
-    }
-    return true;
-}
-
-void placeSquare(int x, int y, int w, std::vector<std::vector<int>>& grid, int value) {
-    // Функция для вставки на соответствующие координаты столешницы квадрата размера w
-    for (int i = x; i < x + w; i++) {
-        for (int j = y; j < y + w; j++) {
-            grid[i][j] = value;
-        }
-    }
-}
-
-std::pair<int, int> findEmptyCell(const std::vector<std::vector<int>>& grid, int n) {
-    // Функция для нахождения по определенному алгоритму пустой клетки
-    for (int x = 0; x < n; x++){
-        // Сначала идет поиск свободного места на на границах столешницы
-        if (grid[x][0] == 0) { // Проверка первого столбца
-            return {x, 0};
-        }
-        if (grid[0][x] == 0) { // Проверка первой строки
-            return {0, x};
-        }
-    }
-    for (int i = 1; i < n; i++) { // Обход всех оставшихся клеток
-        for (int j = 1; j < n; j++) {
-            if (grid[i][j] == 0) {
-                return {i, j};
+        /*int index = 0;
+        for (auto x: automaton){
+            std::cout << index++ << std::endl;
+            std::cout << "Block: " << x.name_of_block << std::endl;
+            for (auto y: x.transitions){
+                std::cout << y.first << " - " << y.second << std::endl;
             }
-            if (grid[j][i] == 0) { // Оптимизация для симметричной проверки
-                return {j, i};
+            for (auto y: x.output){
+                std::cout << y << " ";
             }
-        }
-    }
-    return {-1, -1};
-}
-
-void newStateForStack(int x, int y, int w, const std::vector<std::vector<int>>& grid, const std::vector<std::tuple<int, int, int>>& squaresUsed, std::vector<StackElement>& stack, int n){
-    // Функция для добавления нового состояния в стек
-    std::vector<std::vector<int>> newGrid = grid;
-    placeSquare(x, y, w, newGrid, squaresUsed.size() + 1); // Копирование предыдущего состояния сетки и добавление нового квадрата
-
-    std::vector<std::tuple<int, int, int>> newSquares = squaresUsed;
-    newSquares.push_back({x + 1, y + 1, w}); // Копирование предыдущего состояния вектора квадратов и добавление нового
-    std::pair<int, int> nextEmptyCell = findEmptyCell(newGrid, n);
-
-    stack.push_back({newGrid, newSquares, nextEmptyCell}); // Добавление обновленных полей
-}
-
-std::vector<std::tuple<int, int, int>> solve(int n) {
-    // Функция с алгоритмом решения в виде итеративного бэктрекинга
-    std::vector<std::vector<int>> grid(n, std::vector<int>(n, 0)); // Инициализация матрицы столешницы размера n*n нулями
-    std::vector<StackElement> stack; // Стек, куда будут добавляться все возможные варианты и поочередно рассматриваться
-    std::vector<std::tuple<int, int, int>> bestSolution; // Вектор множества квадратов (координаты и размеры), из которых можно заполнить столешницу
-
-    for (int i = n / 2; i < n; i++) { // Начальное заполнение матрицы максимально большим квадратом
-        newStateForStack(0, 0, i, grid, {}, stack, n);
+            std::cout << "\nFail: " << x.fail << std::endl;
+            std::cout << "=============\n";
+        }*/
     }
 
-    while (!stack.empty()) { // Цикл для обхода всех состояний столешницы
-        StackElement current = stack.back(); // Получение последнего состояния
-        stack.pop_back();
+public:
+    AhoCorasick(const std::vector<std::string>& patterns) : patterns(patterns) {
+        buildTrie();
+        //std::cout << "###############\n";
+        buildFailureLinks();
+    }
 
-        if (!bestSolution.empty() && current.squaresUsed.size() >= bestSolution.size()) {
-            // Если в данном состоянии уже есть перебор по количеству квадратов по сравнению с последним лучшим решением,
-            // происходит выход из этой ветки и переход к следующей
-            std::cout<< "Stop check this state: " << current.squaresUsed.size() << std::endl;
-            printMatrix(current.grid);
-            continue;
-        }
+    std::vector<std::pair<int, int>> search(const std::string& text) {
+        std::vector<std::pair<int, int>> matches;
+        int current = root;
 
-        if (current.emptyCell.first == -1 && current.emptyCell.second == -1) {
-            if (bestSolution.empty() || current.squaresUsed.size() < bestSolution.size()) {
-                // Обновление нового лучшего решения, если столешница заполнена и было затрачено меньше квадратов
-                std::cout<< "Add new best solution: " << current.squaresUsed.size() << std::endl;
-                printMatrix(current.grid);
-                bestSolution = current.squaresUsed;
+        for (int i = 0; i < text.size(); ++i) {
+            char ch = text[i];
+            while (current != -1 && automaton[current].transitions.find(ch) == automaton[current].transitions.end()) {
+                current = automaton[current].fail;
             }
-            continue;
-        }
 
-        int x = current.emptyCell.first;
-        int y = current.emptyCell.second;
-
-        if (x == 0 || y == 0){
-            // Если есть свободные клетки на границах матрицы, идет добавление матрицы максимально возможного размера для оптимизации
-            int w = std::min({n - x, n - y});
-            if (isValid(x, y, w, n, current.grid)) {
-                newStateForStack(x, y, w, current.grid, current.squaresUsed, stack, n);
+            if (current == -1) {
+                current = root;
+            } else {
+                current = automaton[current].transitions[ch];
             }
-            continue;
-        }
 
-        for (int w = 1; w <= std::min({n - x, n - y, n - 1}); w++) {
-            // Если на предыдущем шаге не удалось добавить квадрат,
-            // для свободной клетки идет перебор всех возможных размеров квадрата и добавление этих состояний в стек
-            if (isValid(x, y, w, n, current.grid)) {
-                newStateForStack(x, y, w, current.grid, current.squaresUsed, stack, n);
+            for (int patternIndex : automaton[current].output) {
+                matches.push_back({i + 2 - patterns[patternIndex - 1].size(), patternIndex});
             }
         }
-    }
 
-    // Выход из цикла означает пустоту стека и рассмотрение всех возможных вариантов. Возвращаем лучшее решение
-    return bestSolution;
-}
+        return matches;
+    }
+};
 
 int main() {
-    int n;
-    std::cin >> n; // Чтение входных данных
-    auto solution = solve(n);
+    AhoCorasick ac({"a", "ab", "bab", "bc", "bca", "caa", "c"});
+    std::vector<std::pair<int, int>> matches = ac.search("abccab");
 
-    std::cout << solution.size() << std::endl;
-    for (const auto& square : solution) { // Обход вектора и вывод всех чисел каждого кортежа (x, y, w)
-        std::cout << std::get<0>(square) << " " << std::get<1>(square) << " " << std::get<2>(square) << std::endl;
+    sort(matches.begin(), matches.end());
+
+    for (auto& match : matches) {
+        std::cout << match.first << " " << match.second << std::endl;
     }
+    /*std::string text;
+    int n;
+    std::cin >> text;
+    std::cin >> n;
+    std::vector<std::string> patterns(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> patterns[i];
+    }
+
+    AhoCorasick ac(patterns);
+    std::vector<std::pair<int, int>> matches = ac.search(text);
+
+    sort(matches.begin(), matches.end());
+
+    for (auto& match : matches) {
+        std::cout << match.first << " " << match.second << std::endl;
+    }*/
 
     return 0;
 }
